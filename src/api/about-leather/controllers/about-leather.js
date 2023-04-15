4033ee610ea858f1ae865f5c0fd7059c162e12fa
Andrzej Darowski <andrzej.dar@gmail.com>
2023-04-15 15:47:16 +0200

    init commit

---
+++ src/api/about-leather/controllers/about-leather.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-leather controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::about-leather.about-leather');
---
+++ src/api/about-leather/routes/about-leather.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-leather router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::about-leather.about-leather');
